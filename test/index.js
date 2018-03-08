71cc157026ee936399f6bf6228131fe3bcfae9be
d064437 <toni.ivanisevic@sap.com>
2018-03-08 10:39:53 +0100

    cleaned up files

--- test/index.js
+++
@@ -1,9 +0,0 @@
-var expect = require('expect.js'),
-    bootstrapUpgradeFromV3ToV4 = require('..');
-
-describe('bootstrap-upgrade-from-v-3-to-v-4', function() {
-  it('should say hello', function(done) {
-    expect(bootstrapUpgradeFromV3ToV4()).to.equal('Hello, world');
-    done();
-  });
-});

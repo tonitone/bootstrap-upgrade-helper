7b01410a892d0db7b8350d06236b00db904b537b
d064437 <toni.ivanisevic@sap.com>
2018-03-09 17:42:14 +0100

    working on the regex. still in progress

--- index.js
+++ index.js
@@ -4,9 +4,16 @@
 const replace = require('replace-in-file');
 const path = require('path');
 const fs = require('fs');
+const args = process.argv.slice(2);
+const filesToReplace = args[0];
+const dryRun = args[1] || false;
 
-let selectors = {}, contentAsArray = [], templateEngineVariableStart = '[', templateEngineVariableEnd = ']',
-  transformCSSSelectorToHTMLAttribute, isACSSClassSelector;
+let replaceInFileDefaults = {
+  /*encoding: 'ISO8859-1'*/
+};
+
+let contentAsArray = [], templateEngineVariableStart = '\\[', templateEngineVariableEnd = '\\]',
+  transformCSSSelectorToHTMLAttribute, isACSSClassSelector, getFileListToReplace, isDryRun;
 
 transformCSSSelectorToHTMLAttribute = function (str) {
   return str.replace(".", " ");
@@ -16,19 +23,40 @@ isACSSClassSelector = function (str) {
   return str.charAt(0) === '.';
 };
 
+getFileListToReplace = function () {
+  return filesToReplace;
+};
+
+isDryRun = function () {
+  return !!dryRun;
+};
+
+console.log(isDryRun());
+
 fs.readFile('upgrade-rules.txt.csv', "utf8", function (err, data) {
   contentAsArray = data.split("\n");
   //console.log(contentAsArray)
   //console.log(typeof contentAsArray)
 
 
+  var isLineNotEmptyAndDontStartWithAHash = function () {
+    return oneLine !== '' && oneLine.charAt(0) !== '#';
+  };
+
   for (var oneLine of contentAsArray) {
     oneLine = oneLine.trim();
-    if (oneLine !== '' && oneLine.charAt(0) !== '#') {
+    if (isLineNotEmptyAndDontStartWithAHash()) {
 
-      let searchString = oneLine.split(';')[0].trim(), replaceString = oneLine.split(';')[1].trim();
+      let searchString = oneLine.split(';')[0].trim(),
+        replaceString = oneLine.split(';')[1].trim(),
+        returnSearchStringRegexObject = function (searchString) {
+          searchString = searchString.split('-').join('\-');
 
-      selectors[searchString] = replaceString;
+          // (<.*class=['"].*[^\-\s"'\[])(panel)([^\-\s"'\]]*)
+
+          // + templateEngineVariableStart + searchString + templateEngineVariableEnd +
+          return new RegExp('(class=["\'].*)'+templateEngineVariableStart + searchString + templateEngineVariableEnd +'|(' + searchString + ')(.*["\'])', "g")
+        };
 
       if (isACSSClassSelector(searchString)) {
         searchString = searchString.substr(1);
@@ -39,15 +67,28 @@ fs.readFile('upgrade-rules.txt.csv', "utf8", function (err, data) {
 
       replaceString = transformCSSSelectorToHTMLAttribute(replaceString);
 
-      let options = {
+
+      let replaceInFileOptions = Object.assign({
+        //files: './fixture-bootstrap-3-theme.html',
         files: './fixture-small.html',
-        from: new RegExp('(["|\'].*)(' + searchString + ')(.*["|\'])', "g"),
-        to: '$1' + replaceString + '$3'
-      };
-      //console.log(options);
+        from: returnSearchStringRegexObject(searchString),
+        to: '$1' + replaceString + '$3',
+        dry: isDryRun(),
+      }, replaceInFileDefaults);
+
+
       try {
-        let changes = replace.sync(options);
-        console.log('Modified files:', changes.join(', '));
+        if (searchString === 'panel') {
+          console.log(searchString)
+        }
+        let changes = replace.sync(replaceInFileOptions);
+        console.log(searchString);
+        if (changes.length > 0) {
+          console.log('---');
+          console.log('Modified files:', changes.join(', '));
+          console.log('Search:  ' + returnSearchStringRegexObject(searchString));
+          console.log('Replace: ' + '$1' + replaceString + '$3');
+        }
       }
       catch (error) {
         console.error('Error occurred:', error);
